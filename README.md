0741fa0ba20cccaa0c428d54aa8c790834cfde33
j-hc <j-hc@users.noreply.github.com>
2026-02-17 04:50:30 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ih8SecureLock
+
+Prevent apps from blocking and listening to your screenshots with Zygisk
---
+++ zygisk/jni/parcel.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <stdint.h>
+
+#define ARR_LEN(a) (sizeof(a) / sizeof((a)[0]))
+#define STR_LEN(a) (ARR_LEN(a) - 1)
+
+struct PParcel {
+    size_t error;
+    char* data;
+    size_t data_size;
+};
+
+struct FakeParcel {
+    char* data;
+    uint32_t cur;
+
+    FakeParcel(char* data);
+
+    void skip(uint32_t n);
+    void skipFlatObj();
+
+    uint32_t* peekInt32Ref();
+    uint32_t readInt32();
+    char16_t* readString16(uint32_t len);
+
+    bool checkInterface(const char16_t* desc, uint32_t desc_len);
+};
